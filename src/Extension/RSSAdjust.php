785c688421602b06f88dfafe1a77a33bd20a9a68
Andy Kirk <andy@andykirk.net>
2025-02-10 16:26:35 +0000

    Improvements, fixes.

--- src/Extension/RSSAdjust.php
+++ src/Extension/RSSAdjust.php
@@ -73,6 +73,8 @@ class RSSAdjust extends CMSPlugin implements SubscriberInterface
      */
     public function onAfterRender($options)
     {
+        #return true;
+
         $app = Factory::getApplication();
         if ($app->isClient('administrator')) {
             return; // Don't run in admin
@@ -83,13 +85,32 @@ class RSSAdjust extends CMSPlugin implements SubscriberInterface
             return; // Don't run unless we're dealing with a feed.
         }
 
+        $menu = Factory::getApplication()->getMenu();
+        $menu_item = $menu->getActive();
+
+        $title = $menu_item->title;
+
+        $mi = $menu_item;
+        while ($mi->parent_id > 1) {
+            $mi = $menu->getItem($mi->parent_id);
+        }
+
+        $title .= ' | ' . $mi->title . ' | NPEU';
+
+        #echo '<pre>'; var_dump($title); echo '</pre>'; exit;
+
         $media_namesspace = 'http://search.yahoo.com/mrss/';
 
         $body = $app->getBody();
-        $body = str_replace('<rss', '<rss xmlns:media="' . $media_namesspace . '"', $body);
+        $body = str_replace('<rss', '<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:media="' . $media_namesspace . '"', $body);
         $xml  = new \SimpleXMLElement($body);
 
+        $xml->channel->title = $title;
         $article_model = $app->bootComponent('com_content')->getMVCFactory()->createModel('Article', 'Administrator', ['ignore_request' => true]);
+        $logo = $xml->channel->addChild('image');
+        $logo->addChild('url', URI::root() . 'assets/images/brand-logos/unit/npeu-device-logo.png');
+        $logo->addChild('title', 'NPEU');
+        $logo->addChild('link', 'https://www.npeu.ox.ac.uk/');
         foreach($xml->channel->item as $item) {
             $url_parts = explode('/', $item->link);
             $url_last = array_pop($url_parts);
@@ -105,10 +126,20 @@ class RSSAdjust extends CMSPlugin implements SubscriberInterface
             $article = $article_model->getItem($id);
 
             $fields = FieldsHelper::getFields('com_content.article',  $article, true);
+
+            if (empty($fields)) {
+                continue;
+            }
+
             if ($fields[0]->name == 'headline-image' && !empty($fields[0]->rawvalue)) {
                 $img_data = json_decode($fields[0]->rawvalue);
-                $img_url = URI::root() . preg_replace('/#.*/', '', $img_data->imagefile) . '?s=300';
 
+                // Older version of Joomla stored the custom field image differently:
+                if ($img_data) {
+                    $img_url = URI::root() . preg_replace('/#.*/', '', $img_data->imagefile) . '?s=300';
+                } else {
+                    $img_url = URI::root() . $fields[0]->rawvalue;
+                }
                 $image = $item->addChild('media:thumbnail', null, $media_namesspace);
                 $image->addAttribute('url', $img_url);
             }
